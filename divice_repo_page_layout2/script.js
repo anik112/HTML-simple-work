eb4d2963a3ed7bcdc8566edc8a1cf9afaf265a2d
anik112 <paulanik112@gmail.com>
2018-09-21 10:38:17 +0600

    make page layout with device repo

---
+++ divice_repo_page_layout2/script.js
@@ -0,0 +1,7 @@
+function size(){
+    var nav_main=document.getElementById("side-nav").className = "side-nav mod-nav";
+}
+
+function off_size(){
+    var nav_main=document.getElementById("side-nav").className = "off-mode";
+}
