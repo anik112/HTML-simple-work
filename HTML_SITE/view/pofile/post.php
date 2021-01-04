adcdd39fb6aed151371135d3da8e289b8def9789
anik112 <paulanik112@gmail.com>
2021-01-05 00:36:32 +0600

    Update new file

--- HTML_SITE/view/pofile/post.php
+++ HTML_SITE/view/pofile/post.php
@@ -8,49 +8,40 @@
                         <div class="card-body">
                         
                         <?php
-                        $post_content=""; 
-                        $file_name="";
-                        if(isset($_POST["post"])){
+                        $post_content="";
+                        $target_file="";
+                        $file_tmp;
+                        if(isset($_POST["submit"])){
                             if(isset($_POST["post-content"])){
                                 $post_content=$_POST["post-content"];
-                                echo $post_content;
                             }
                             
-                            if(isset($_FILES["post-image"])){
-                                $file_name=$_FILES["post-image"]["tmp_name"];
-                                echo $file_name;
+                            if(isset($_FILES["images"])){
                                 $target_dir = "uploads/";
-                                $file=$_FILES["post-image"];
-                                echo $file;
-                                $target_file = basename($_FILES["post-image"]["name"]);
-                                echo $target_file;
+                                $file_tmp=$_FILES['images']['tmp_name'];
+                                $file=$_FILES["images"];
+                                $target_file = basename($_FILES["images"]["name"]);
+                                move_uploaded_file($file_tmp,"./images/$target_file");
                             }
-                            
 
-                            
-                            
-
-                            //$sqlInsertPost="INSERT INTO `tb_posts`( `user_id`, `user_name`, `content`, `imsge`,  `likes`, `comment`) VALUES ($activeId,'$userName','$post_content','../images/$file_name',0,0)";
-                            //$inserData= $connect->prepare($sqlInsertPost);
-                            //$inserData->execute();
-
-                            //move_uploaded_file($file,"../images/$file_name");
+                            $sqlInsertPost="INSERT INTO `tb_posts`( `user_id`, `user_name`, `content`, `imsge`,  `likes`, `comment`) VALUES ($activeId,'$userName','$post_content','./images/$target_file',0,0)";
+                            $inserData= $connect->prepare($sqlInsertPost);
+                            $inserData->execute();
                         }
-
 
                         ?>
 
                         <div>
-                        <form class="border rounded border-primary p-4" method="POST" action="<?php $_PHP_SELF ?>">
+                        <form class="border rounded border-primary p-4" method="POST" action="" enctype="multipart/form-data">
                             <div class="form-group">
                                 <textarea name="post-content" id="post-content" rows="3" placeholder="Type content" class="form-control p-2"></textarea>
                                 <small id="emailHelp" class="form-text text-muted">Please share the valid information.</small>
                             </div>
                             <div class="form-group">
                                 <label for="post-image">Choose Post image</label>
-                                <input type="file" name="post-image" class="form-control-file" id="post-image">
+                                <input type="file" name="images" class="form-control-file" id="post-image">
                             </div>
-                            <button type="submit" name="post" class="btn btn-primary" style="width: 200px">Postes</button>
+                            <button type="submit" name="submit" class="btn btn-primary" style="width: 200px">Postes</button>
                         </form>
 
                         </div>
@@ -64,7 +55,18 @@
                 // get comment from comment table
                 $getComments=getDataUsingOrderAndId($connect,'`tb_comments_list`',$post->id,'post_id');
                 $commentCount=0;
-                    ?>
+                $likeCount=0;
+
+                $sqlgetcomment="SELECT COMMENT FROM `tb_posts` WHERE id=$post->id";
+                $inserData=$connect->prepare($sqlgetcomment);
+                $inserData->execute();
+                $commentCount=$inserData->fetchAll(PDO::FETCH_OBJ);
+
+                $sqlgetlike="SELECT likes FROM `tb_posts` WHERE id=$post->id";
+                $updateData=$connect->prepare($sqlgetlike);
+                $updateData->execute();
+                $likeCount=$updateData->fetchAll(PDO::FETCH_OBJ);
+                ?>
 
                             <div class="card my-3">
                                 <div class="card-header" id="post-head">
@@ -87,11 +89,11 @@
                                     <button class="btn btn-light" onclick="changeLoveIconInPost('post-love-icon<?php echo $comId;?>')"
                                         style="float: left; padding: 2px; margin: 2px;" id="btn-post-love">
                                         <img src="./image/love-inactive.png" alt="" srcset="" class="post-love-icon" id="post-love-icon<?php echo $comId;?>">
-                                        12 Love this
+                                        <?php echo $likeCount[0]->likes; ?> Love this
                                     </button>
                                     <button class="btn btn-light" onclick="toggleBtn('comments<?php echo $comId; ?>')"
                                         style="float: right; padding: 2px; margin: 2px;">
-                                        12 Comments
+                                        <?php echo $commentCount[0]->COMMENT;?> Comments
                                         <img src="./image/comment.png" alt="" srcset="" id="post-comment-icon">
                                     </button>
                                 </div>
